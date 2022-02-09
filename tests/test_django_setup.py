bbb211010bbd537dc71de0ba37777a88adb36684
David Nugent <david.nugent@news.com.au>
2022-02-10 10:42:43 +1100

    Add test for verbose

--- tests/test_django_setup.py
+++ tests/test_django_setup.py
@@ -79,3 +79,29 @@ def test_settings_commandline2():
     assert not settings['mobile']
     assert settings['site_id'] == 96
     assert settings['site_ui'] == 16
+
+
+def test_settings_commandline_verbose(capsys):
+    prog, env, settings = common_commandline(['test', '-v', '-e', 'localdev', '-n', 'bss', '-u', '16', '-o', 'extra'])
+
+    assert prog == 'test'
+    assert len(sys.argv) == 2
+    assert sys.argv[1] == 'extra'
+
+    assert settings['env'] == 'localdev'
+    assert settings['noweb']
+    assert not settings['mobile']
+    assert settings['site_id'] == 96
+    assert settings['site_ui'] == 16
+
+    captured = capsys.readouterr()
+    assert captured.err == """\
+SITE_ID=96
+SITE_UI=16
+DJANGO_NETWORK=bss
+DJANGO_ENVIRON=localdev
+DJANGO_READ_DOT_ENV_FILE=False
+NOT_WEB_MODE=True
+RUN_AS_MOBILE=False
+DJANGO_SETTINGS=tests.settings.default
+"""
